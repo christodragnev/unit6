4dd56bcde2f9af4d6525a63d27b82933e1c1de6b
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-09 08:39:13 -0400

    Updated from Brython Server: 5/9/2018 8:39:13 AM

--- fileDemo.py
+++ fileDemo.py
@@ -1,1 +1,12 @@
+#Christo Dragnev
+#5/9/18
+#fileDemo.py - how to read a file
 
+file = open('engmix.txt')
+
+numWords = 0
+for line in file:
+    if 'smed' in line:
+        print(line.strip())
+    numWords +=1
+print(numWords)
