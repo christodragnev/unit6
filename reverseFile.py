21cfc9d1ada250c362d3b3f1e8bdb0ed779122b0
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-10 08:47:51 -0400

    Updated from Brython Server: 5/10/2018 8:47:51 AM

--- reverseFile.py
+++ reverseFile.py
@@ -1,1 +1,16 @@
+#Christo Dragnev
+#5/10/18
+#reverseFile.py
 
+file = input('Enter file name: ')
+
+open('file')
+
+new = []
+for line in file:
+    if len(line)>0:
+        new.append(line)
+print(new)
+    
+    
+    
