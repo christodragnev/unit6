0b63b56e54d38be25c3a233d295899113aae5efb
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-10 08:37:05 -0400

    Updated from Brython Server: 5/10/2018 8:37:04 AM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,14 @@
+#Christo Dragnev
+#5/10/18
+#howManyWords.py
 
+file = open('engmix.txt')
+
+L = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+for word in file:
+    L[len(word)-1] = L[len(word)-1] + 1
+    
+i = 1
+while i>=22:
+    print(i,'-letter words:',L[i])
+    i+=1
