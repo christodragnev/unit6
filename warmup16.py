d1a33f138cd9a72f35f5039139707b8edcaa4a90
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-11 08:28:21 -0400

    Updated from Brython Server: 5/11/2018 8:28:20 AM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,12 @@
+#Christo Dragnev
+#5/12/18
+#warmup16.py - prints out all words that start with your initial and end with your last inital
 
+file = open('engmix.txt')
+
+for line in file:
+    line = line.strip()
+    if len(line) > 0:
+        if 'c' in line[0] and 'o' in line[-1]:
+            print(line)
+        
