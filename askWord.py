0868b5f33675ae9318cd2e9700b74b8f84ada0f5
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-14 08:22:16 -0400

    Updated from Brython Server: 5/14/2018 8:22:15 AM

--- askWord.py
+++ askWord.py
@@ -1,1 +1,15 @@
+file = open('engmix.txt')
+word = input('Enter a word: ')
 
+end = 0
+while end == 0:
+    for line in file:
+        if word == line.strip():
+            print(word, 'is in the dictionary')
+            end+=1
+        elif word != line.strip():
+            print(word, 'is not in the dictionary')
+            end+=1
+        
+        
+            
