ffbcf513e8d9178ef366f79614d97db5e0200df7
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-17 08:49:09 -0400

    Updated from Brython Server: 5/17/2018 8:49:09 AM

--- longShort.py
+++ longShort.py
@@ -1,1 +1,12 @@
+#Christo Dragnev
+#5/17/18
+#longShort.py
 
+file = open('engmix.txt')
+
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+L = []
+for line in file:
+    line = line.strip()
+    L.append(line)
