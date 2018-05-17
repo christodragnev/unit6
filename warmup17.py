9a531ba71523b8236014c65e5940d77616500089
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-17 08:17:55 -0400

    Updated from Brython Server: 5/17/2018 8:17:55 AM

--- warmup17.py
+++ warmup17.py
@@ -1,1 +1,10 @@
+#Christo Dragnev
+#5/17/18
+#warmup17.py - find all words that contain every letter of your last name
 
+file = open('engmix.txt')
+
+for line in file:
+    line = line.strip()
+    if 'd' in line and 'r' in line and 'a' in line and 'g' in line and 'n' in line and 'e' in line and 'v' in line:
+        print(line)
