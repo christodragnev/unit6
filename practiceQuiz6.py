04816a3044aecaabfc76251b4fdcb13e9bc38578
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-18 08:31:27 -0400

    Updated from Brython Server: 5/18/2018 8:31:26 AM

--- practiceQuiz6.py
+++ practiceQuiz6.py
@@ -1,1 +1,12 @@
+#Christo Dragnev
+#5/18/18
+#practiceQuiz6.py
 
+#Program1
+file = open('engmix.txt')
+
+for line in file:
+    line = line.strip()
+    if line.count('c')==3 and line.count('p')==2:
+        print(line)
+        
