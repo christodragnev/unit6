b451d51c1bd93d6600bdba2dbcd50097eae14ff2
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-18 08:49:10 -0400

    Updated from Brython Server: 5/18/2018 8:49:08 AM

--- practiceQuiz6.py
+++ practiceQuiz6.py
@@ -5,24 +5,26 @@
 #Program1
 file = open('engmix.txt')
 
-'''
+
 for line in file:
     line = line.strip()
     if line.count('c')==3 and line.count('p')==2:
-        print(line)'''
+        print(line)
+
         
 #program3
 num = int(input('Enter a number: '))
-'''
+
 for line in file:
     line = line.strip()
     if len(line) == num:
         print(line)
-        break'''
+        break
         
 #program4
 letter = input('Enter a letter: ')
 
 for line in file:
     line = line.strip()
-    for 
+    for letter not in line:
+        print(line.count())
