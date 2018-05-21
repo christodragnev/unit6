9b3f91de51006bddd331226b727c4a8238c6584d
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-21 08:07:19 -0400

    Updated from Brython Server: 5/21/2018 8:07:18 AM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,20 @@
+#Christo Dragnev
+#5/21/18
+#quiz6.py
 
+file = open('engmix.txt')
+
+#1
+"""
+letter = input('Enter a letter: ')
+for line in file:
+    line = line.strip()
+    if line.count(letter)==4:
+        print(line)
+"""
+
+#2
+new = []
+for line in file:
+    line = line.strip()
+    if len(line) > 0:
