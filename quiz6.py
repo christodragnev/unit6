3d7ac84e946dcb5f0f95aab066dc8a9d85b5de53
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-21 08:15:50 -0400

    Updated from Brython Server: 5/21/2018 8:15:50 AM

--- quiz6.py
+++ quiz6.py
@@ -14,7 +14,34 @@ for line in file:
 """
 
 #2
+'''
 new = []
 for line in file:
     line = line.strip()
     if len(line) > 0:
+        new.append(line)
+        if line[0] == line[4] and line[8] == line[4]:
+            print(line)
+'''
+        
+#3
+'''
+num = int(input('Enter a number: '))
+letter = input('Enter a letter: ')
+new = []
+for line in file:
+    line = line.strip()
+    if len(line) > 0:
+        new.append(line)
+        if letter == line[0] and num == len(line):
+            print(line)
+'''
+
+#4
+new = []
+for line in file:
+    line = line.strip()
+    if len(line) >= 10:
+        new.append(line)
+    print(new[7999])
+    
