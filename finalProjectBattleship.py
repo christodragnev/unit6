194d6b693118250e748c3814ea7afd2ef0b6404c
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-23 08:39:35 -0400

    Updated from Brython Server: 5/23/2018 8:39:35 AM

--- finalProjectBattleship.py
+++ finalProjectBattleship.py
@@ -1,1 +1,18 @@
+#Christo Dragnev
+#5/23/18
+#battleshipGraphics.py
 
+
+from ggame import *
+
+red = Color(0xFF0000,1) #this is the color red
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+
+def buildboard():
+    blackOutline = LineStyle(1,black)
+    blueCircle = CircleAsset(30,blackOutline,blue) #radius, outline, full
+    Sprite(blueCircle,(25,25))
+    App().run()
+print(buildboard())
