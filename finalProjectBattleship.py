0bbd8b0904104fe9d426556733fb26da609401d6
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-24 08:50:02 -0400

    Updated from Brython Server: 5/24/2018 8:50:02 AM

--- finalProjectBattleship.py
+++ finalProjectBattleship.py
@@ -9,16 +9,16 @@ white = Color(0xFFFFFF,1)
 black = Color(0x000000,1)
 blackOutline = LineStyle(1,black)
 
-circleRadius = 20
+radius = 30
 
 def buildboard():
     board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 
 def redrawAll():
-    circle = CircleAsset(circleRadius,blackOutline,white)
+    circle = CircleAsset(radius,blackOutline,white)
     for r in range(0,5):
         for c in range(0,5):
-            Sprite(circle,(10+r*(2*circleRadius+10),10+(2*circleRadius+10)*c))
+            Sprite(circle,(10+r*(2*radius),10+(2*radius)*c))
     for item in App().spritelist[:]:
         item.destroy()
 
