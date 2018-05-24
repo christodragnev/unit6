9b0b87719004503052391e6aaca1c92fa550e039
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-24 08:39:01 -0400

    Updated from Brython Server: 5/24/2018 8:39:01 AM

--- finalProjectBattleship.py
+++ finalProjectBattleship.py
@@ -5,17 +5,19 @@
 
 from ggame import *
 
-red = Color(0xFF0000,1) #this is the color red
-green = Color(0x00FF00,1)
-blue = Color(0x0000FF,1)
+white = Color(0xFFFFFF,1)
 black = Color(0x000000,1)
+blackOutline = LineStyle(1,black)
 
 def buildboard():
-    blackOutline = LineStyle(1,black)
-    blueCircle = CircleAsset(30,blackOutline,blue) #radius, outline, full
-    Sprite(blueCircle,(25,25))
+    board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+
+def redrawAll():
+    circle = CircleAsset(30,blackOutline,white)
+    for r in range(0,5):
+        for c in range(0,5):
+            Sprite(circle,(10+r*(2*25+10),10+(2*25+10)*c))
+
+if __name__ == '__main__':
+    redrawAll()
     App().run()
-
-board = [['a','b','c','d','e'],['f','g','h','i','j'],['k','l','m','n','o'],['p','q','r','s','t'],['u','v','w','x','y']]
-
-print(buildboard())
