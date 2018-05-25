ad35a77ecbbc649d69e3864603154ae20ed7e168
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-05-25 08:45:08 -0400

    Updated from Brython Server: 5/25/2018 8:45:09 AM

--- finalProjectBattleship.py
+++ finalProjectBattleship.py
@@ -4,6 +4,7 @@
 
 
 from ggame import *
+from random import randint
 
 white = Color(0xFFFFFF,1)
 black = Color(0x000000,1)
@@ -15,13 +16,31 @@ def buildboard():
     board = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 
 def redrawAll():
+    for item in App().spritelist[:]:
+        item.destroy()
     circle = CircleAsset(radius,blackOutline,white)
     for r in range(0,5):
         for c in range(0,5):
             Sprite(circle,(10+r*(2*radius),10+(2*radius)*c))
-    for item in App().spritelist[:]:
-        item.destroy()
+            
+    for r in range(0,5):
+        for c in range(0,5):
+            Sprite(circle,(500+r*(2*radius),10+(2*radius)*c))
+            
+
+def pickComputerShips():
+    return
+
+def mouseClick(event):
+    blackCircle = CircleAsset(radius,blackOutline,black)
+    if (event.x < 60 and event.y < 60) and (event.x>10 and event.y >10):
+            Sprite(blackCircle,(10,10))
+
+    
+
 
 if __name__ == '__main__':
+    
+    App().listenMouseEvent('click',mouseClick)
     redrawAll()
     App().run()
