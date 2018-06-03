cbe06375783d7f8a2396fdc975c849a3ec5207f2
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-06-03 18:24:58 -0400

    Updated from Brython Server: 6/3/2018 6:24:58 PM

--- finalProjectBattleship.py
+++ finalProjectBattleship.py
@@ -9,38 +9,49 @@ from random import randint
 white = Color(0xFFFFFF,1)
 black = Color(0x000000,1)
 red = Color(0xFF0000,1)
+blue = Color(0x009AFF, 1)
 blackOutline = LineStyle(1,black)
 
 EMPTY = 0
 MISS = 1
 HIT = 2
-
 radius = 30
 
 def buildboard():
     return [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
 
 
-def redrawAll():
+def redrawAll(): 
     for item in App().spritelist[:]:
         item.destroy()
-    circle = CircleAsset(radius,blackOutline,white)
-    blackCircle = CircleAsset(radius,blackOutline,black)
-    redCircle = CircleAsset(radius,blackOutline,red)
+        
+    emptyCircle = CircleAsset(radius,blackOutline,white)
+    shipCircle = CircleAsset(radius,blackOutline,black)
+    hitCircle = CircleAsset(radius,blackOutline,red)
+    missCircle = CircleAsset(radius,blackOutline,blue)
+    
     for r in range(0,5):
         for c in range(0,5):
-            Sprite(circle,(10+r*(2*radius),10+(2*radius)*c))
-            if data['board'][r][c]=='ship' and data['playerships']<=3:
-                Sprite(blackCircle,(10+r*(2*radius),10+(2*radius)*c))
+            if data['board'][r][c] == 0:
+                Sprite(emptyCircle,(10+r*(2*radius),10+(2*radius)*c))
+            elif data['board'][r][c] == 'ship':
+                Sprite(shipCircle,(10+r*(2*radius),10+(2*radius)*c))
+            elif data['board'][r][c] == MISS:
+                Sprite(missCircle,(10+r*(2*radius),10+(2*radius)*c))
+            elif data['board'][r][c] == HIT:
+                Sprite(hitCircle,(10+r*(2*radius),10+(2*radius)*c))
             
     for r in range(0,5):
         for c in range(0,5):
-                Sprite(circle,(500+r*(2*radius),10+(2*radius)*c))
-                if data['compboard'][r][c]=='compship':
-                    Sprite(blackCircle,(500+r*(2*radius),10+(2*radius)*c))
-                if data['compboard'][r][c]=='guess':
-                    Sprite(redCircle,(500+r*(2*radius),10+(2*radius)*c))
-            
+                if data['compboard'][r][c] == 0:
+                    Sprite(emptyCircle,(500+r*(2*radius),10+(2*radius)*c))
+                elif data['compboard'][r][c]=='compship':
+                    Sprite(shipCircle,(500+r*(2*radius),10+(2*radius)*c))
+                elif data['compboard'][r][c] == MISS:
+                    Sprite(missCircle,(500+r*(2*radius),10+(2*radius)*c))
+                elif data['compboard'][r][c] == HIT:
+                    Sprite(missCircle,(500+r*(2*radius),10+(2*radius)*c))
+                
             
 
 def pickComputerShips():
@@ -55,33 +66,53 @@ def pickComputerShips():
 def computerTurn():
     row = randint(0,4)
     col = randint(0,4)
-    if data['turn']==False:
-        if data['board'][row][col]==0:
-            data['board'][row][col]='ship'
-    
+    if data['board'][row][col] == 'ship':
+        data['playerBoard'][row][col] = HIT
+        data['playerHits'] += 1
+        if data['playerHits'] == 3:
+            data['endGame'] = True
+            print('Computer wins!')
+        redrawAll()
+    elif data['board'][row][col] == 0:
+        data['board'][row][col] = MISS
+        redrawAll()
+    else:
+        computerTurn()
 
 def mouseClick(event):
     row = int(event.x//60)
     col = int(event.y//60)
-    data['board'][row][col]='ship'
-    data['playerships']+=1
-    if data['turn']==True and data['playerships']==4:
-        data['compboard'][row][col]='guess'
-        
-        
-        
-    redrawAll()
-
+    if data['end']==False:
+        if  data['playerships']<3:
+            data['board'][row][col]='ship'
+            data['playerships']+=1
+        elif data['playerships']==3:
+            if data['compboard'][(event.x-500)//60][col] == 'ship':
+                    data['compboard'][(event.x-500)//60][col] = HIT
+                    data['computerHits'] += 1
+                    if data['computerHits'] == 3:
+                        data['end'] = True
+                        print('Player wins!')
+                    else:
+                        computerTurn()
+                    redrawAll()
+        elif data['compboard'][(event.x-500)//60][col] == 0:
+                    data['compboard'][(event.x-500)//60][col] = MISS
+                    computerTurn()
+                    redrawAll()
             
+    redrawAll()
     
-
-
 if __name__ == '__main__':
     
     data = {}
     data['board'] = buildboard()
     data['compboard'] = buildboard()
     data['playerships'] = 0
+    data['playerHits'] = 0
+    data['computerHits'] = 0
+    data['end'] = False
+    
     
     data['turn']=True
     
